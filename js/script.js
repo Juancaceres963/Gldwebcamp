0b656f160d63861f25398237301f31b4e317b46b
Juancaceres963 <dnijuancaceres@gmail.com>
2023-02-21 11:56:32 -0300

    Funcion calcular montos

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 (function(){
     "use strict";
 
+    var regalo = document.getElementById("regalo");
+
     document.addEventListener("DOMContentLoaded", function(){
 // Campos datos de usuario
 var nombre = document.getElementById("nombre");
@@ -17,6 +19,23 @@ const calcular = document.getElementById("calcular");
 const errorDiv = document.getElementById("error");
 const bottonRegistro = document.getElementById("btnRegistro");
 const resultado = document.getElementById("lista_productos");
-        console.log(nombre);
+
+//Funciones 
+//Calcular Montos 
+function calcularMontos(event){
+    event.preventDefault();
+    if(regalo.value === ""){
+        alert("Debes elegir un Regalo");
+        regalo.focus();
+    } else {
+        var valorPaseDia = pase_dia.value ,
+            valorPaseDosDias = pase_dos_dias.value ,
+            valorPaseCompleto = pase_completo.value;
+
+        var totalPrecioPases = (valorPaseDia * 30) + (valorPaseDosDias * 45) + (valorPaseCompleto * 50);
+        console.log(totalPrecioPases);
+    }
+}
+calcular.addEventListener("click", calcularMontos)
     }); //Don content Loaded
 })();

d460c3e6942afa2faefa611efd33162a377435fe
Juancaceres963 <dnijuancaceres@gmail.com>
2023-02-21 11:29:44 -0300

    Seleccionando elementos creando variables

---
+++ js/script.js
@@ -0,0 +1,22 @@
+(function(){
+    "use strict";
+
+    document.addEventListener("DOMContentLoaded", function(){
+// Campos datos de usuario
+var nombre = document.getElementById("nombre");
+var apellido = document.getElementById("apellido");
+var email = document.getElementById("email");
+
+// Campos pases
+var pase_dia = document.getElementById("pase_dia");
+var pase_dos_dias = document.getElementById("pase_dos_dias");
+var pase_completo = document.getElementById("pase_completo");
+
+//Botones y divs
+const calcular = document.getElementById("calcular");
+const errorDiv = document.getElementById("error");
+const bottonRegistro = document.getElementById("btnRegistro");
+const resultado = document.getElementById("lista_productos");
+        console.log(nombre);
+    }); //Don content Loaded
+})();

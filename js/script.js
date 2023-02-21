e2842f10563f05170442c8c03c0110432f78cbd1
Juancaceres963 <dnijuancaceres@gmail.com>
2023-02-21 13:38:41 -0300

    Listado de productos, Resumen innerHTML

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ var pase_completo = document.getElementById("pase_completo");
 const calcular = document.getElementById("calcular");
 const errorDiv = document.getElementById("error");
 const bottonRegistro = document.getElementById("btnRegistro");
-const resultado = document.getElementById("lista_productos");
+const lista_productos = document.getElementById("lista_productos");
 
 //Extras
 var etiquetas = document.getElementById("etiquetas");
@@ -32,14 +32,39 @@ function calcularMontos(event){
         alert("Debes elegir un Regalo");
         regalo.focus();
     } else {
-        var valorPaseDia = pase_dia.value ,
-            valorPaseDosDias = pase_dos_dias.value ,
-            valorPaseCompleto = pase_completo.value,
-            cantCamisas = camisas.value,
-            cantEtiquetas = etiquetas.value;
+        // ParseInt se utiliza para que evitemos posibles numeros raros que puedan salir
+        var valorPaseDia = parseInt(pase_dia.value, 10)|| 0 ,
+            valorPaseDosDias = parseInt(pase_dos_dias.value, 10)|| 0  ,
+            valorPaseCompleto = parseInt(pase_completo.value, 10)|| 0 ,
+            cantCamisas = parseInt(camisas.value, 10)|| 0 ,
+            cantEtiquetas = parseInt(etiquetas.value, 10)|| 0 ;
 
-        var totalPrecioPases = (valorPaseDia * 30) + (valorPaseDosDias * 45) + (valorPaseCompleto * 50) + ((cantCamisas * 10) * .93) + (cantEtiquetas * 2);
-        console.log(totalPrecioPases);
+        var totalPagar = (valorPaseDia * 30) + (valorPaseDosDias * 45) + (valorPaseCompleto * 50) + ((cantCamisas * 10) * .93) + (cantEtiquetas * 2);
+        
+        var listadoProductos = [];
+        if(valorPaseDia >= 1){
+            listadoProductos.push(valorPaseDia + " Pase por dia")
+        }
+        if(valorPaseDosDias >= 1){
+            listadoProductos.push(valorPaseDosDias + " Pase por dos dias")
+        }
+        if(valorPaseCompleto >= 1){
+            listadoProductos.push(valorPaseCompleto + " Pase completo")
+        }
+        if(cantCamisas >= 1){
+            listadoProductos.push(cantCamisas + " Camisetas")
+        }
+        if(cantEtiquetas >= 1){
+            listadoProductos.push(cantEtiquetas + " Etiquetas")
+        }
+
+        // Codigo para imprimir los elementos de listadoProductos en el div lista_productos que esta en el HTML
+        lista_productos.innerHTML = ""; // Este innerHTML vacio es para que no reimprima todo lo que se selecciono
+        // si el usuario agrega algo mas despues de calcular
+        for (var i = 0; i < listadoProductos.length; i++){
+            lista_productos.innerHTML += listadoProductos[i] +"<br/>";
+        }
+
     }
 }
 calcular.addEventListener("click", calcularMontos)
